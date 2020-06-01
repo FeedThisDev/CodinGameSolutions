c6dbaa947a554cb38eb07f0b0fdf9eefa663e0c0
Till Dittrich <till@feedthis.dev>
2020-06-01 23:08:39 +0200

    only test immediate surrounding runes

--- Puzzles/Puzzles/CodeOfRings.cs
+++ Puzzles/Puzzles/CodeOfRings.cs
@@ -8,6 +8,12 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Net;
 
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+
 public struct Gamestate
 {
     public int TotalStringLength { get; set; }
@@ -24,12 +30,14 @@ public class Node
 
     public Node Parent { get; set; }
 }
-
 class Player
 {
+
+    static string MagicPhrase;
     static int PermCounter = 0;
     static char[] runeValues = " ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
-    static char[] runes = new char[30];
+    static Dictionary<char, int> runeLetterToIndex = new Dictionary<char, int>();
+
     #region MagicStrings
     public static string[] goRight = {
     "",
@@ -137,57 +145,112 @@ class Player
 
     #endregion
 
-    const int MaxTake = 6;
-    const int TreeDeph = 5;
-    static string MagicPhrase = "THREE RINGS FOR THE ELVEN KINGS UNDER THE SKY SEVEN FOR THE DWARF LORDS IN THEIR HALLS OF STONE NINE FOR MORTAL MEN DOOMED TO DIE ONE FOR THE DARK LORD ON HIS DARK THRONEIN THE LAND OF MORDOR WHERE THE SHADOWS LIE ONE RING TO RULE THEM ALL ONE RING TO FIND THEM ONE RING TO BRING THEM ALL AND IN THE DARKNESS BIND THEM IN THE LAND OF MORDOR WHERE THE SHADOWS LIE";
-    //public static string MagicPhrase = Console.ReadLine(); // "UMNE TALMAR RAHTAINE NIXENEN UMIR";
+    static int MaxTake = 4;
+    static int TreeDeph = 6;
+    static int SurroundingRadius = 4;
+
     static void Main(string[] args)
     {
-        Stopwatch sw = new Stopwatch();
-        sw.Start();
 
+        for (int i = 0; i < runeValues.Length; i++)
+        {
+            runeLetterToIndex.Add(runeValues[i], i);
+        }
+
+        //for (int j = 1; j < 7; j++)
+        //{
+        //    for (int jj = 1; jj < 7; jj++)
+        //    {
+        //        for (int jjj = 1; jjj < 7; jjj++)
+        //        {
+
+
+                     //MaxTake = j;
+                     //TreeDeph = jj;
+                     //SurroundingRadius = jjj;
+
+
+                    Stopwatch sw = new Stopwatch();
+        sw.Start();
+        StringBuilder resultBuilder = new StringBuilder();
+        char[] runes = new char[30];
         for (int i = 0; i < 30; i++)
             runes[i] = ' ';
 
-        var letterToCount2 = MagicPhrase.ToCharArray().GroupBy(x => x, x => x, (key, g) => new { Letter = key, Count = g.Count() }).ToDictionary(key => key.Letter, val => val.Count);
-        int spaces = 30 - letterToCount2.Count;
-        var letterToCount = letterToCount2.OrderByDescending(p => p.Value);
 
-        var letterToRune = new Dictionary<char, int>();
-        StringBuilder resultBuilder = new StringBuilder();
-        int playerPosition = 0;
-        int runeIdx = 0;
-        foreach (var kvp in letterToCount)
+        //MagicPhrase = "THREE RINGS FOR THE ELVEN KINGS UNDER THE SKY SEVEN FOR THE DWARF LORDS IN THEIR HALLS OF STONE NINE FOR MORTAL MEN DOOMED TO DIE ONE FOR THE DARK LORD ON HIS DARK THRONEIN THE LAND OF MORDOR WHERE THE SHADOWS LIE ONE RING TO RULE THEM ALL ONE RING TO FIND THEM ONE RING TO BRING THEM ALL AND IN THE DARKNESS BIND THEM IN THE LAND OF MORDOR WHERE THE SHADOWS LIE";
+        MagicPhrase = Console.ReadLine(); // "UMNE TALMAR RAHTAINE NIXENEN UMIR";
+
+
+        Gamestate beginning = new Gamestate()
         {
-            resultBuilder.Append(GetMoveToRune(playerPosition, runeIdx));
-            resultBuilder.Append(StartChangeEmptyRuneTo(kvp.Key));
-            letterToRune.Add(kvp.Key, runeIdx);
-            playerPosition = runeIdx;
-            runeIdx++;
+            CurrentRunes = runes,
+            TotalStringLength = 0,
+            PlayerPosition = 0,
+        };
+
+        Node rootNode = new Node()
+        {
+            State = beginning,
+            Parent = null
+        };
+
+        int idx = 0;
+        while (idx < MagicPhrase.Length - TreeDeph)
+        {
+            var shortestNode = FillTree(rootNode, idx, idx + TreeDeph);
+            idx += TreeDeph;
+            resultBuilder.Append(shortestNode.State.TotalString);
+            rootNode = new Node
+            {
+                Parent = null,
+                State = new Gamestate
+                {
+                    CurrentRunes = shortestNode.State.CurrentRunes,
+                    PlayerPosition = shortestNode.State.PlayerPosition,
+                    TotalStringLength = 0,
+                    TotalString = ""
+                }
+            };
         }
 
-        for (int i = 0; i < MagicPhrase.Length; i++)
+        var finalNode = FillTree(rootNode, idx, MagicPhrase.Length);
+        resultBuilder.Append(finalNode.State.TotalString);
+
+        sw.Stop();
+                    //Console.Error.WriteLine($"{MaxTake} {TreeDeph} {SurroundingRadius}: {sw.ElapsedMilliseconds}ms {resultBuilder.Length}");
+                    //Console.Error.WriteLine(PermCounter);
+                    //Console.Error.WriteLine(resultBuilder.Length);
+                    Console.WriteLine(resultBuilder.ToString());
+        //        }
+        //    }
+        //}
+
+        //Console.WriteLine(shortestNode.State.TotalString.ToString());
+        //Console.ReadKey();
+    }
+
+    private static List<int> GetSurroundingPositions(int playerPosition)
+    {
+        List<int> result = new List<int>();
+        for (int i = playerPosition - SurroundingRadius; i < playerPosition + SurroundingRadius; i++)
         {
-            if (!letterToRune.ContainsKey(MagicPhrase[i]))
+            if (i < 0)
             {
-                resultBuilder.Append(GetMoveToRune(playerPosition, runeIdx));
-                resultBuilder.Append(StartChangeEmptyRuneTo(MagicPhrase[i]));
-                letterToRune.Add(MagicPhrase[i], runeIdx);
-                playerPosition = runeIdx;
-                runeIdx++;
+                result.Add(i + 30);
+            }
+            else if(i >= 30)
+            {
+                result.Add(i - 30);
             }
             else
             {
-                int newPlayerPosition = letterToRune[MagicPhrase[i]];
-                resultBuilder.Append(GetMoveToRune(playerPosition, newPlayerPosition) + ".");
-                playerPosition = newPlayerPosition;
+                result.Add(i);
             }
         }
-
-        Console.Error.WriteLine(resultBuilder.Length);
-        Console.WriteLine(resultBuilder.ToString());
-        Console.ReadKey();
+        return result;
     }
+
 
     private static Node FillTree(Node thisNode, int currentPhraseIndex, int length)
     {
@@ -222,7 +285,7 @@ class Player
     {
         List<Gamestate> possibleResultingGamestate = new List<Gamestate>();
 
-        for (int i = 0; i < 30; i++)
+        foreach(var i in GetSurroundingPositions(currentState.PlayerPosition))
         {
             string moveTo = GetMoveToRune(currentState.PlayerPosition, i);
             string changeTo = ChangeAndHitRune(i, letter, currentState.CurrentRunes, out char[] newRunes);
@@ -240,63 +303,15 @@ class Player
         return possibleResultingGamestate.OrderBy(x => x.TotalStringLength).Take(MaxTake).ToArray();
     }
 
-    public static string StartChangeEmptyRuneTo(char letter)
-    {
-        int indexDestinationLetter = 0;
-        while (runeValues[indexDestinationLetter] != letter)
-            indexDestinationLetter++;
-
-        int indexCurrentLetter = 0;
-
-        int offsetForwards = 0;
-        int offsetBackwards = 0;
-
-        if (indexDestinationLetter > indexCurrentLetter)
-        {
-            offsetForwards = Math.Abs(indexDestinationLetter - indexCurrentLetter);
-            offsetBackwards = runeValues.Length - Math.Abs(indexCurrentLetter - indexDestinationLetter);
-        }
-        else
-        {
-            offsetForwards = runeValues.Length - Math.Abs(indexCurrentLetter - indexDestinationLetter);
-            offsetBackwards = Math.Abs(indexDestinationLetter - indexCurrentLetter);
-        }
-
-
-        if (offsetBackwards > offsetForwards)
-            return add[offsetForwards];
-        return substract[offsetBackwards];
-    }
-
     public static string ChangeAndHitRune(int i, char nextLetter, char[] runes, out char[] newRunes)
     {
-        int indexDestinationLetter = 0;
-        while (runeValues[indexDestinationLetter] != nextLetter)
-            indexDestinationLetter++;
-
-        int indexCurrentLetter = 0;
-        while (runeValues[indexCurrentLetter] != runes[i])
-            indexCurrentLetter++;
-
-        int offsetForwards = 0;
-        int offsetBackwards = 0;
-
-        if (indexDestinationLetter > indexCurrentLetter)
-        {
-            offsetForwards = Math.Abs(indexDestinationLetter - indexCurrentLetter);
-            offsetBackwards = runeValues.Length - Math.Abs(indexCurrentLetter - indexDestinationLetter);
-        }
-        else
-        {
-            offsetForwards = runeValues.Length - Math.Abs(indexCurrentLetter - indexDestinationLetter);
-            offsetBackwards = Math.Abs(indexDestinationLetter - indexCurrentLetter);
-        }
+        int offset = GetLetterDifference(nextLetter, runes[i]);
         newRunes = (char[])runes.Clone();
         newRunes[i] = nextLetter;
 
-        if (offsetBackwards > offsetForwards)
-            return add[offsetForwards] + '.';
-        return substract[offsetBackwards] + '.';
+        if (offset >= 0)
+            return add[offset] + '.';
+        return substract[-offset] + '.';
     }
 
     public static string GetMoveToRune(int currentPosition, int destinationPos)
@@ -320,4 +335,29 @@ class Player
 
         return goLeft[offsetBackwards];
     }
+
+    public static int GetLetterDifference(char letter1,char letter2)
+    {
+        int indexCurrentLetter = runeLetterToIndex[letter2];
+        int indexDestinationLetter = runeLetterToIndex[letter1];
+
+        int offsetForwards = 0;
+        int offsetBackwards = 0;
+
+        if (indexDestinationLetter > indexCurrentLetter)
+        {
+            offsetForwards = Math.Abs(indexDestinationLetter - indexCurrentLetter);
+            offsetBackwards = runeValues.Length - Math.Abs(indexCurrentLetter - indexDestinationLetter);
+        }
+        else
+        {
+            offsetForwards = runeValues.Length - Math.Abs(indexCurrentLetter - indexDestinationLetter);
+            offsetBackwards = Math.Abs(indexDestinationLetter - indexCurrentLetter);
+        }
+
+        if (offsetBackwards > offsetForwards)
+            return offsetForwards;
+        return -offsetBackwards;
+    }
+
 }
